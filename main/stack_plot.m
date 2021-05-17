7cad3dc418ec57003f7f50ee8eb5c4a435b527df
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-05-17 10:27:56 -0400

    Update stack_plot.m

--- main/stack_plot.m
+++ main/stack_plot.m
@@ -2,11 +2,11 @@ function stack_plot(DeltaFoverF)
 
 x = length(DeltaFoverF(1,:));
 y = length(DeltaFoverF(:,1));
-baseline = max(DeltaFoverF,[],'all');
+baseline = .75*max(DeltaFoverF,[],'all');
 for i = 1:y
     gradient = i/y;
-    plot(DeltaFoverF(i,:)+(0.7*baseline),'LineWidth',1,'Color',[.30 .835 .384]); hold on;
-    baseline = baseline + max(DeltaFoverF,[],'all');
+    plot(DeltaFoverF(i,:)+(0.5*baseline),'LineWidth',1,'Color',[.30 .835 .384]); hold on;
+    baseline = baseline + .75*max(DeltaFoverF,[],'all');
 end
 
 end
