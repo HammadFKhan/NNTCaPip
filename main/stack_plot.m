9a63cab89bfbb32a2f516215a864917587ed6085
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-05-20 11:28:11 -0400

    Functional spacing adjustment

--- main/stack_plot.m
+++ main/stack_plot.m
@@ -1,12 +1,16 @@
-function stack_plot(DeltaFoverF)
-
+function stack_plot(DeltaFoverF,spacing)
+if nargin < 2 || strcmp(spacing,'')
+    spacing = 1;
+end
 x = length(DeltaFoverF(1,:));
 y = length(DeltaFoverF(:,1));
-baseline = .75*max(DeltaFoverF,[],'all');
+baseline = spacing*max(DeltaFoverF,[],'all');
 for i = 1:y
     gradient = i/y;
-    plot(DeltaFoverF(i,:)+(0.5*baseline),'LineWidth',1,'Color',[.30 .835 .384]); hold on;
-    baseline = baseline + .75*max(DeltaFoverF,[],'all');
+    plot(DeltaFoverF(i,:)+(spacing*baseline),'LineWidth',1,'Color',[.30 .835 .384]); hold on;
+    baseline = baseline + spacing*max(DeltaFoverF,[],'all');
+    axis tight, box off,...
+            set(gca,'YTick','','YTickLabel','');
 end
 
 end
