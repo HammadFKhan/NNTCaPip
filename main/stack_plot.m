5fb749ae945b3a31cc76d4c402ae7ef223da1d94
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-10-12 15:31:38 -0400

    Update stack_plot.m

--- main/stack_plot.m
+++ main/stack_plot.m
@@ -1,4 +1,7 @@
-function stack_plot(DeltaFoverF,spacing)
+function stack_plot(DeltaFoverF,spacing,amplitude)
+if nargin < 3 || strcmp(amplitude,'')
+    amplitude = 1;
+end
 if nargin < 2 || strcmp(spacing,'')
     spacing = 1;
 end
@@ -7,7 +10,7 @@ y = length(DeltaFoverF(:,1));
 baseline = spacing*max(DeltaFoverF,[],'all');
 for i = 1:y
     gradient = i/y;
-    plot(DeltaFoverF(i,:)+(spacing*baseline),'LineWidth',2,'Color',[.30 .835 .384]); hold on;
+    plot(amplitude*DeltaFoverF(i,:)+(spacing*baseline),'LineWidth',1,'Color',[.30 .835 .384]); hold on;
     baseline = baseline + spacing*max(DeltaFoverF,[],'all');
     axis tight, box off,...
             set(gca,'YTick','','YTickLabel','');
