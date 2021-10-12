19546fc76d1b3b3277d432ff61f29518d066f3a2
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-10-12 15:31:26 -0400

    Update Connectivity_dice.m

--- main/Connectivity_dice.m
+++ main/Connectivity_dice.m
@@ -6,7 +6,7 @@ for m=1:length(ROI)
     for j = 1:length(ROI)
         if m>j
             rhoVal = corr(j,m);
-            if rhoVal >= 0.25
+            if rhoVal >= 0.15
                 Connected_ROI(L,:) = [j,m,corr(j,m)];
                 L = L + 1;
             end
