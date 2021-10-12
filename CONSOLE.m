ae70a7f134a38467c02358ef2232ff42a53a46bf
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-10-12 15:31:12 -0400

    Update CONSOLE.m

--- CONSOLE.m
+++ CONSOLE.m
@@ -36,7 +36,7 @@ set(0,'DefaultFigureWindowStyle','docked')
 addpath(genpath('main'));
 cell_count = length(ROI);
 time = time_adjust(num_images,15);
-std_threshold = 5;
+std_threshold = 3;
 static_threshold = .2;
 Spikes = Spike_Detector_Single(dDeltaFoverF,std_threshold,static_threshold);
 corr = correlation_dice(Spikes);
@@ -49,11 +49,11 @@ bin = 20; %Vector sizes for similarity indexing (Num frames should be devisable 
 Spikes_shuffled = tempShuffle(Spikes,num_images,cell_count);
 Event_shuffled = spatialShuffle(Spikes,num_images,cell_count);
 surrogate = 10;
-Total_shuffled = allShuffle(Spikes,num_images,cell_count,surrogate);
+Total_shuffled = allShuffle(Spikes,surrogate);
 shuff_corr = correlation_dice(Total_shuffled);
 
-[vectorized,sim_index] = cosine_similarity(Spikes,bin);
-[shufvectorized,shufsim_index] = cosine_similarity(Total_shuffled,bin);
+[vectorized,sim_index] = cosine_similarity(Spikes(:,1:2000),bin);
+[shufvectorized,shufsim_index] = cosine_similarity(Total_shuffled(:,1:2000),bin);
 shufsim_index = shufsim_index-mean(mean(shufsim_index,2));
 [NumActiveNodes,NumNodes,NumEdges,SpatialCentroid,SpatialCentroidVariance,...
     ActivityCentroid,ActivityCentroidVariance]...
@@ -63,43 +63,35 @@ a = mean(mean(sim_index,2));
 b = mean(abs(mean(shufsim_index,2)));
 %% Trial by Trial analysis ##Only use with batch processed files##
 addpath(genpath('Figures'));
-[batchSpikes,batch_corr] = TrialByTrial(batchData([1,4,5,6,7,8])); % Function call
+[batchSpikes,batch_corr] = TrialByTrial(batchData([1,2,4])); % Function call
 bin = 20;
 [vectorized,sim_index] = cosine_similarity(batchSpikes,bin);
 [z,mu,sigma] = zscore(sim_index);
-figure('Name','Cosine-Similarity Index'); h = htmp(sim_index);
-% caxis([.5 0.9]);
+figure('Name','Cosine-Similarity Index'); h = htmp(sim_index,100);
+caxis([0 0.7]);
 set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);...
-     print(gcf,'-painters','-depsc', 'Figures/CSTrials.eps', '-r250');
+     print(gcf,'-painters','-depsc2', 'Figures/CSTrials.eps', '-r250');
  figure('Name','Dice Correlation')
  for i = 1:size(batch_corr,3)
      subplot(2,3,i),h = htmp(batch_corr(:,:,i),20);caxis([0 0.4]);
  end
 %% Plot all the Figures
 addpath('Figures');
-figure('Name','DeltaF/F'); stack_plot(DeltaFoverF,0.8); 
+figure('Name','DeltaF/F'); stack_plot(DeltaFoverF,0.8,8); 
 % figure('Name','Convolved Spikes'); plot(dDeltaFoverF');
 % figure('Name','Threshold Detection');DeltaFoverFplotter(dDeltaFoverF,std_threshold,static_threshold)
 figure('Name','Spike Plot'); spikePlot = Show_Spikes(Spikes);
-figure('Name','Temporal Shuffled Spike Plot'); shuffledTspikePlot = Show_Spikes(Spikes_shuffled);
-figure('Name','Event Shuffled Spike Plot'); shuffledEspikePlot = Show_Spikes(Event_shuffled);
-figure('Name','Total Shuffled Spike Plot'); shuffledAspikePlot = Show_Spikes(Total_shuffled);
-figure('Name','Fluorescence Map'); spikeImage = spike_map(DeltaFoverF,time);caxis([0 .4]);...
-    print(gcf,'-painters','-depsc', 'Figures/Fluormap.eps', '-r250');
+% figure('Name','Temporal Shuffled Spike Plot'); shuffledTspikePlot = Show_Spikes(Spikes_shuffled);
+% figure('Name','Event Shuffled Spike Plot'); shuffledEspikePlot = Show_Spikes(Event_shuffled);
+% figure('Name','Total Shuffled Spike Plot'); shuffledAspikePlot = Show_Spikes(Total_shuffled);
+figure('Name','Fluorescence Map'); spikeImage = spike_map(DeltaFoverF,time);caxis([0 .4]);
 figure('Name','Population Intensity');height = 10;rateImage = firing_rate(Spikes,height,time);caxis([0 0.5]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);...
-    print(gcf,'-painters','-depsc', 'Figures/Population.eps', '-r250');
-figure('Name','Coactivity Index'); B = bar(time,coactive_cells);...
-    ax = gca;ax.TickDir = 'out';ax.Box = 'off';axis off;...
-    print(gcf,'-painters','-depsc', 'Figures/Coactive.eps', '-r250');
-figure('Name','Dice-Similarity Index');h = htmp(corr,10);caxis([0 0.4]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);...
-     print(gcf,'-painters','-depsc', 'Figures/DSC.eps', '-r250');
-figure('Name','Shuffled Dice-Similarity Index');h = htmp(shuff_corr,10);caxis([0 0.4]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);...
-     print(gcf,'-painters','-depsc', 'Figures/sDCS.eps', '-r250');
-figure('Name','Cosine-Similarity Index'); h = htmp(sim_index);caxis([0.35 .9]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);...
-     print(gcf,'-painters','-depsc', 'Figures/CS.eps', '-r250');
-figure('Name','Shuffled Cosine-Similarity Index'); h = htmp(shufsim_index);caxis([0 1]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);...
-     print(gcf,'-painters','-depsc', 'Figures/sCS.eps', '-r250');
-figure('Name','Network Map'); NodeSize = 6;EdgeSize = 2;Cell_Map_Dice(AverageImage,Connected_ROI,ROIcentroid,NodeSize,EdgeSize)
+figure('Name','Coactivity Index'); B = bar(time,coactive_cells);ax = gca;ax.TickDir = 'out';ax.Box = 'off';axis off;
+figure('Name','Dice-Similarity Index');h = htmp(corr,10);caxis([0 0.3]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);...
+figure('Name','Shuffled Dice-Similarity Index');h = htmp(shuff_corr,10);caxis([0 0.4]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
+figure('Name','Cosine-Similarity Index'); h = htmp(sim_index);caxis([0.35 .9]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
+figure('Name','Shuffled Cosine-Similarity Index'); h = htmp(shufsim_index);caxis([0 1]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
+figure('Name','Network Map'); NodeSize = 3;EdgeSize = 2;Cell_Map_Dice(AverageImage,Connected_ROI,ROIcentroid,NodeSize,EdgeSize)
 
 %% Rotary Encoder
 figure('Name','Pulse Data');plot(encoder_data.rotate_pulse);
