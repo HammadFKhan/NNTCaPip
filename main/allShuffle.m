2bf62740f66c7a750083419529ebf322bc50397d
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-10-12 15:31:16 -0400

    Update allShuffle.m

--- main/allShuffle.m
+++ main/allShuffle.m
@@ -1,4 +1,7 @@
-function Total_shuffled = allShuffle(Spikes,num_images,cell_count,surrogate)
+function Total_shuffled = allShuffle(Spikes,surrogate)
+if nargin<2 || strcmp(surrogate,''), surrogate = 5; end
+cell_count = size(Spikes,1);
+num_images = size(Spikes,2);
 Total_shuffled = zeros(cell_count,num_images);
 for x = 1:surrogate
     p = randperm(num_images);
