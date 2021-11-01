4018091630d2c335f2e8cac3abfab84022587e9a
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-11-01 17:47:50 -0400

    Update coactive_index.m

--- main/coactive_index.m
+++ main/coactive_index.m
@@ -1,10 +1,12 @@
-function [coactive_cells,detected_spikes] = coactive_index(Spikes)
+function [coactive_cells,detected_spikes] = coactive_index(Spikes,bin)
 for i = 1:length(Spikes(1,:))
     total = length(Spikes(:,i));
     detected_spikes(i) = sum(Spikes(:,i));
     coactive_cells(:,i) = detected_spikes(i)./total;
 end
-coactive_cells = coactive_cells.*100;
+bins = discretize(1:length(coactive_cells(1,:)),bin); 
+for i = 1:bins(end)
+bin_coactive(i) = sum(coactive_cells(find(bins==i)));
 end
-
+coactive_cells = bin_coactive;
     
