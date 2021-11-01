31186709c9597af9c5355fd632b57c7da67a448b
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-11-01 17:47:23 -0400

    Update Cell_Map_Dice.m

--- main/Cell_Map_Dice.m
+++ main/Cell_Map_Dice.m
@@ -1,7 +1,7 @@
-function Cell_Map_Dice(AverageImage,Connected_ROI,ROIbases,NodeSize,EdgeSize)
+function Cell_Map_Dice(AverageImage,Connected_ROI,ROIbases,NodeList,NodeSize,EdgeSize)
 
-if nargin < 5; Edgesize = 3;end
-if nargin < 4; NodeSize = 6;end
+if nargin < 6; Edgesize = 5;end
+if nargin < 5; NodeSize = 5;end
 
 
 % Centroid = regionprops(AverageImage,'centroid');
@@ -19,12 +19,12 @@ background = ones(H,W);
 
 [lengthConnected,~] = size(Connected_ROI);
 
-for i = 1:length(centroids)
+for i = 1:length(NodeList)
 %     plot(centroids(i,1),centroids(i,2), 'r+', 'MarkerSize', 10, 'LineWidth', 2);
 
     radius = NodeSize; 
-    centerX = centroids(i,1);
-    centerY = centroids(i,2);
+    centerX = centroids(NodeList(i),1);
+    centerY = centroids(NodeList(i),2);
 %     circle = viscircles([centerX, centerY], radius);
     for x = 1:H
         for y = 1:W
@@ -39,32 +39,46 @@ for i = 1:length(centroids)
 end
 
 imshow(background)
-map = [0.02 0.631 0.631
+map = [0.02 0.631 0.8
     1 1 1];
 colormap(map);
 hold on
 %%
-
-
 if Connected_ROI(1,:) ~= [0,0,0]
     for j = 1:lengthConnected
         Cell1 = ROIbases(Connected_ROI(j,1),:);
         Cell2 = ROIbases(Connected_ROI(j,2),:);
         corr = Connected_ROI(j,3);
-        if corr > 0.5
-            LineWidth = 2*EdgeSize;
-            Color = [0.05 0.2 0.76];
-        elseif corr >= 0.1 && corr <= 0.5
+        if corr > 0.6
             LineWidth = EdgeSize;
             Color = [0.02 0.631 0.8];
+            x1 = Cell1(1,1);
+            x2 = Cell2(1,1);
+            y1 = Cell1(1,2);
+            y2 = Cell2(1,2);
+            %         Color = [0.02 0.631 0.631];
+            %         Color = 'k';
+            x = line([y1,y2],[x1,x2],'LineWidth',LineWidth,'Color',Color);
+            x.Color(4) = 0.6;
+%         elseif corr >= 0.15 && corr <= 0.6
+%             LineWidth = EdgeSize;
+%             Color = [.83 0.45 0.05];
+%             x1 = Cell1(1,1);
+%             x2 = Cell2(1,1);
+%             y1 = Cell1(1,2);
+%             y2 = Cell2(1,2);
+%             %         Color = [0.02 0.631 0.631];
+%             %         Color = 'k';
+%             x = line([y1,y2],[x1,x2],'LineWidth',LineWidth,'Color',Color);
+%             
         end
-        x1 = Cell1(1,1);
-        x2 = Cell2(1,1);
-        y1 = Cell1(1,2);
-        y2 = Cell2(1,2);
-%         Color = [0.02 0.631 0.631];
-%         Color = 'k';
-        x = line([y1,y2],[x1,x2],'LineWidth',LineWidth,'Color',Color);
-        x.Color(4) = 0.4;
+        %         x1 = Cell1(1,1);
+        %         x2 = Cell2(1,1);
+        %         y1 = Cell1(1,2);
+        %         y2 = Cell2(1,2);
+        % %         Color = [0.02 0.631 0.631];
+        % %         Color = 'k';
+        %         x = line([y1,y2],[x1,x2],'LineWidth',LineWidth,'Color',Color);
+        %         x.Color(4) = 0.5;
     end
 end
