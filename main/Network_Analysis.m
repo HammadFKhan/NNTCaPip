3a97cd376effc9be45d5a001a759411b4d105bf7
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-11-01 17:47:33 -0400

    Update Network_Analysis.m

--- main/Network_Analysis.m
+++ main/Network_Analysis.m
@@ -1,5 +1,6 @@
-function [NumActiveNodes,NumNodes,NumEdges,SpatialCentroid,SpatialCentroidVariance,ActivityCentroid,ActivityCentroidVariance] = Network_Analysis(ROIcentroids,Connected_ROI)
+function [NumActiveNodes,NodeList,NumNodes,NumEdges,SpatialCentroid,SpatialCentroidVariance,ActivityCentroid,ActivityCentroidVariance] = Network_Analysis(ROIcentroids,Connected_ROI)
 NumActiveNodes=0;
+NodeList = 0;
 NumNodes=size(ROIcentroids,1);
 SpatialCentroid = 'Null';
 SpatialCentroidVariance = 'Null';
@@ -12,31 +13,21 @@ if Connected_ROI(1,3) ~= 0
     Cell_1=Connected_ROI(1,1);
     Cell_2=Connected_ROI(1,2);
     NodeList=[Cell_1,Cell_2];
-    index=3;
     %Count all Nodes with Edges
     if size(Connected_ROI,1) > 1
-        for j = 2:size(Connected_ROI,1)
-            Cell_1=Connected_ROI(j,1);
-            Cell_2=Connected_ROI(j,2);
-            if ismember(Cell_1,NodeList)== 0 
-                NodeList(index)=Cell_1;
-                index=index+1;
-            elseif ismember(Cell_2,NodeList)==0
-                NodeList(index)=Cell_2;
-                index=index+1;
-            end
-        end
-        NumActiveNodes=length(NodeList);
-    %Find Centroid of Edges
-        ActivityCoords=zeros(size(Connected_ROI,1),2);
-         for i=1:size(Connected_ROI,1)
-            coord_Cell_1=ROIcentroids(Connected_ROI(i,1),:);
-            coord_Cell_2=ROIcentroids(Connected_ROI(i,2),:);
-            ActivityCoords(i,:) = (coord_Cell_1+coord_Cell_2)./2;
-         end
-        ActivityCentroid = mean(ActivityCoords,1);
-        ActivityCentroidVariance=std(ActivityCoords,0,1);
+        NodeList = vertcat(Connected_ROI(:,1),Connected_ROI(:,2));
+        NodeList = unique(NodeList);
     end
+    NumActiveNodes=length(NodeList);
+    %Find Centroid of Edges
+    ActivityCoords=zeros(size(Connected_ROI,1),2);
+    for i=1:size(Connected_ROI,1)
+        coord_Cell_1=ROIcentroids(Connected_ROI(i,1),:);
+        coord_Cell_2=ROIcentroids(Connected_ROI(i,2),:);
+        ActivityCoords(i,:) = (coord_Cell_1+coord_Cell_2)./2;
+    end
+    ActivityCentroid = mean(ActivityCoords,1);
+    ActivityCentroidVariance=std(ActivityCoords,0,1);
 else
     NumActiveNodes=0;
     NumNodes=0;
