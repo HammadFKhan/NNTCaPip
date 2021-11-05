c06d5f98d54787d009a0b22f7647f93a6724458c
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-11-04 23:10:13 -0400

    Update htmp.m

--- main/htmp.m
+++ main/htmp.m
@@ -5,7 +5,7 @@ h = imagesc(corr);
 % map = [0.02 0.631 0.631;
 %     1 1 1];
 % h.Colormap(map);
-% [grad,~]=colorGradient([.1 .1 .1],[1 1 1],128);
+% [grad,~]=colorGradient([0 0 0],[1 1 1],2);
 % colormap(grad);
 colormap(jet);
 set(get(colorbar,'title'),'string','\Delta F/F (%)');
