b011e77f51b8c1ef766e1f448944f802de0d2e0b
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-11-10 14:45:15 -0500

    Update CONSOLE.m

--- CONSOLE.m
+++ CONSOLE.m
@@ -1,18 +1,6 @@
 %% Run the CaimAn program
 % Github Version 4.1
 
-%####Batch Analysis for ROI Extaction####
-addpath(genpath('main'));
-clear
-clc
-close all;
-tic
-CaImAn_Conversion
-disp('Batch Analysis complete')
-toc
-%% Batch Data Analysis
-addpath(genpath('main'));
-batchData = batchData_Analysis;
 %% CaimAn Single File ROI Extraction 
 clear
 clc
@@ -22,15 +10,7 @@ addpath(genpath('main'));
 tic
 Start_CaImAn
 toc
-%% CaimAn Single File ROI Extraction from Dendrites
-clear
-clc
-close all;
-set(0,'DefaultFigureWindowStyle','normal')
-addpath(genpath('main'));
-tic
-Start_CaImAn_Dendrites
-toc
+
 %% Single File Analysis
 set(0,'DefaultFigureWindowStyle','docked')
 addpath(genpath('main'));
@@ -43,43 +23,33 @@ Spikes = Spike_Detector_Single(dDeltaFoverF,std_threshold,static_threshold);
 calcium_avg = STA(DeltaFoverF,Spikes,std_threshold,5);
 bin = 10; %Vector sizes for similarity indexing (Num frames should be devisable by this)           
 
-Spikes_shuffled = tempShuffle(Spikes,10000);
+% Spikes_shuffled = tempShuffle(Spikes,10000);
 % Event_shuffled = spatialShuffle(Spikes,10000);
 % surrogate = 10;
 % Total_shuffled = allShuffle(Spikes,10000);
-[shufcoactive_cells,detected_spikes] = coactive_index(Spikes_shuffled,length(Spikes_shuffled));
-shuff_corr = correlation_dice(Event_shuffled);
+% [shufcoactive_cells,detected_spikes] = coactive_index(Spikes_shuffled,length(Spikes_shuffled));
+% shuff_corr = correlation_dice(Event_shuffled);
 % [shufvectorized,shufsim_index] = cosine_similarity(Total_shuffled,bin);
 % shufsim_index = shufsim_index-mean(mean(shufsim_index,2));
 
-%%
-[vectorized,sim_index] = cosine_similarity(Spikes(:,1:4180),bin);
+[vectorized,sim_index] = cosine_similarity(Spikes(:,1:1800),bin);
 corr = correlation_dice(Spikes);
 Connected_ROI = Connectivity_dice(corr, ROI);
 [NumActiveNodes,NodeList,NumNodes,NumEdges,SpatialCentroid,SpatialCentroidVariance,...
     ActivityCentroid,ActivityCentroidVariance]...
     = Network_Analysis(ROIcentroid,Connected_ROI);
-%% Rank Correlation
-rank = mean(corr);
-[~,idx_sort] = sort(rank);
-rankCorr = corr(idx_sort,:);
-%%
-idx = find(trig==1); %Find when the trigger is high
-onset = find(diff(idx)>1)+1; %Find when the trigger ends
-events(:,1) = idx(:,1)/20000; %Take only the first value of the trigger event
-for i = 1:length(onset)
-    events(:,i+1) = idx(:,onset(:,i))/20000; % Map the onset of the pulse in seconds
+%% Ensemble Analysis
+Ensemble = ensembleAnalysis(Spikes,ROI,ROIcentroid);
+%% Plot Ensemble
+for i = 1:Ensemble.ensembleIndentified 
+    figure(1),
+    EnsembleMap(AverageImage,ROIcentroid,Ensemble.NodeList{2},5)
+    set(gcf, 'PaperSize', [4 4]);
+    drawnow
+    hold on
 end
-%%
-Peristim{1} = Spikes(:,1:120);
-for i = 1:floor(4182/120)-1 % Extract calcium data +-2s to stim (120 frames per trial) number should equal trials
-    Peristim{i+1} = Spikes(:,(i*120)+1:((i+1)*120));
-end
-Peristim{ceil(4182/120)} = Spikes(:,(i+1)*120:end); %Account for differences at the end of the trail
-%%
-for i = 1:length(ensembleCan)
-    ensemble(:,i) = stimTrials(:,ensembleCan(i));
-end
+
+% Combine Maps
 
 %% Ensemble Analysis
 % win = find(t1==1);
@@ -89,66 +59,8 @@ end
 countS = 1;
 j = 6;
 bins = discretize(1:length(Peristim{j}(1,:)),60); %~13 frames
-for j = 3
-    for i = 1:8
-        disp(['Detecting Ensembles in bin: '  num2str(i)])
-        bins = discretize(1:length(Peristim{j}(1,:)),8); %~13 frames
-        corr = correlation_dice(Peristim{j});
-        %             avgCorr(:,i) = mean((mean(corr)));
-        %             [coactive_cells,detected_spikes] = coactive_index(lateSpike{j},30);
-                        Connected_ROI{i} = Connectivity_dice(corr, ROI);
-                        [NumActiveNodes(j,i),NodeList,NumNodes(j,i),NumEdges(j,i),SpatialCentroid,SpatialCentroidVariance,...
-                            ActivityCentroid,ActivityCentroidVariance]...
-                            = Network_Analysis(ROIcentroid,Connected_ROI);
-        %         Extract active nodes to graph
-        %                 id = find(Connected_ROI(:,3)>0.6);
-        %                 ensembleNodes = [Connected_ROI(id,1) Connected_ROI(id,2)];
-        %                 strongConnections(j,i) = length(find(Connected_ROI(:,3)>0.5));
-        %                 weakConnections(j,i) = length(find(Connected_ROI(:,3)<0.5));
-        %     figure(1),h = htmp(corr);caxis([0 1]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
-        %                     figure(fh1), subplot(2,4,i),NodeSize = 6;EdgeSize = 3;Cell_Map_Dice(AverageImage,Connected_ROI,ROIcentroid,NodeSize,EdgeSize);
-        %             title(['Time: ' num2str(j) ' seconds']);
-        %                     count = count+1;
-        %         corr = correlation_dice(ensemble(:,80:100));
-        %             Connected_ROI = Connectivity_dice(corr, ROI);
-        %             [NumActiveNodes,NodeList,NumNodes,NumEdges,SpatialCentroid,SpatialCentroidVariance,...
-        %                 ActivityCentroid,ActivityCentroidVariance]...
-        %                 = Network_Analysis(ROIcentroid,Connected_ROI);
-        %             hold on;
-        %             figure(2),
-        %             EnsembleMap(AverageImage,ROIcentroid,NodeList,NodeSize)
-        %     drawnow
-        %             hold off;
-    end
-end
-%% %% Ensemble Analysis
-% win = find(t1==1);
-% UDS = find(diff(win)>1)
-% fh1 = figure;
-% fh2 = figure;
-countS = 1;
-j = 6;
-bins = discretize(1:length(Peristim{j}(1,:)),60); %~13 frames
 for j = 1:16
     disp(['Detecting Ensembles in bin: '  num2str(i)])
-    bins = discretize(1:length(weaklateSpike{j}(1,:)),8); %~13 frames
-    corr = correlation_dice(weaklateSpike{j});
-    %             avgCorr(:,i) = mean((mean(corr)));
-    %             [coactive_cells,detected_spikes] = coactive_index(lateSpike{j},30);
-    %                 Connected_ROI{i} = Connectivity_dice(corr, ROI);
-    %                 [NumActiveNodes(j,i),NodeList,NumNodes(j,i),NumEdges(j,i),SpatialCentroid,SpatialCentroidVariance,...
-    %                     ActivityCentroid,ActivityCentroidVariance]...
-    %                     = Network_Analysis(ROIcentroid,Connected_ROI);
-    %         Extract active nodes to graph
-    %                 id = find(Connected_ROI(:,3)>0.6);
-    %                 ensembleNodes = [Connected_ROI(id,1) Connected_ROI(id,2)];
-    %                 strongConnections(j,i) = length(find(Connected_ROI(:,3)>0.5));
-    %                 weakConnections(j,i) = length(find(Connected_ROI(:,3)<0.5));
-%     figure(1),h = htmp(corr);caxis([0 1]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
-    %                     figure(fh1), subplot(2,4,i),NodeSize = 6;EdgeSize = 3;Cell_Map_Dice(AverageImage,Connected_ROI,ROIcentroid,NodeSize,EdgeSize);
-%             title(['Time: ' num2str(j) ' seconds']);
-    %                     count = count+1;
-    %         corr = correlation_dice(ensemble(:,80:100));
             Connected_ROI = Connectivity_dice(corr, ROI);
             [NumActiveNodes,NodeList,NumNodes,NumEdges,SpatialCentroid,SpatialCentroidVariance,...
                 ActivityCentroid,ActivityCentroidVariance]...
@@ -159,89 +71,7 @@ for j = 1:16
     drawnow
             hold off;
 end
-%%
-for j = 1:length(Peristim)
-    for i = 2:4
-        stack{j,i-1} = Peristim{j}(:,find(bins==i));
-    end
-    responseCorrelation{j} = horzcat(stack{j,:});
-end
-responseCorrelation = cat(2,responseCorrelation{:});
-%%
-corr = correlation_dice(ensemble(:,80:100));
-Connected_ROI = Connectivity_dice(corr, ROI);
-[NumActiveNodes,NodeList,NumNodes,NumEdges,SpatialCentroid,SpatialCentroidVariance,...
-                    ActivityCentroid,ActivityCentroidVariance]...
-                    = Network_Analysis(ROIcentroid,Connected_ROI);
-figure,
-EnsembleMap(AverageImage,ROIcentroid,NodeList,NodeSize)
-%% Concatenate no stim trails
-for i = 1:length(allTrials)
-    nostimTrials{i} = Peristim{allTrials(i,1)}(:,end-44:end);
-end
-nostimTrials = horzcat(nostimTrials{:});
-%% Population Activity before and after stim
-[vectorized,sim_index] = cosine_similarity(Spikes(:,1:4175),25);
-% figure,imagesc(vectorized),colormap(jet),colorbar
-figure('Name','Cosine-Similarity Index'); h = htmp(sim_index);caxis([0.35 .9]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
-[vectorized,sim_index] = cosine_similarity(nostimTrials,25);
-figure,imagesc(vectorized),colormap(jet),colorbar
-% figure('Name','Cosine-Similarity Index'); h = htmp(sim_index);caxis([0.35 .9]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
-% figure('Name','Fluorescence Map'); spikeImage = spike_map(stimTrials,time);caxis([0 1]);
-% figure('Name','Fluorescence Map'); spikeImage = spike_map(nostimTrials,time);caxis([0 .1]);
-%%
-corr = correlation_dice(stimTrials);
-Connected_ROI = Connectivity_dice(corr, ROI);
-figure('Name','Dice-Similarity Index');h = htmp(corr,10);caxis([0 0.3]);set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
-[NumActiveNodes,NodeList,NumNodes,NumEdges,SpatialCentroid,SpatialCentroidVariance,...
-    ActivityCentroid,ActivityCentroidVariance]...
-    = Network_Analysis(ROIcentroid,Connected_ROI);
-figure,
-EnsembleMap(AverageImage,ROIcentroid,NodeList,5);
-%%
-ensembleCan = find(coactive_cells>0.13);
-for i = 1:length(ensembleCan)
-ensemble(:,i) = Spikes(:,ensembleCan(i));
-end
-%% 
-[vectorized,sim_index] = cosine_similarity(ensemble(:,1:190),2);
-svd_analysis(sim_index)
 
-%%
-% Strong vs Weak input response
-% Checks the stong vs weak connection 1-2 seconds after pole swing
-countS = 1;
-countW = 1;
-for i = 1:length(LateSpikeTrials)
-        weakLateResponse(i,:) = [strongConnections(CountS,:), weakConnections(WeakLateSpikeTrials(i,:),:)];
-end
-
-%% 
-trial = 1:12;
-figure
-plot(mean(LateResponse(trial,1:8),1)), hold on;
-plot(mean(LateResponse(trial,9:16,1)))
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
-trial = 1:16;
-figure
-plot(mean(weakLateResponse(trial,1:8),1)), hold on;
-plot(mean(weakLateResponse(trial,9:16,1)))
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
-%%
-figure,plot(strongConnections(:,8))
-hold on,plot(weakConnections(:,8))
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
-
-%%
-figure
-for i = 1:size(strongConnections,1)
-    plot(strongConnections(i,:)','Color',[0.2 0.2 0.2 i/25]), hold on;
-end
-figure
-for i = 1:size(strongConnections,1)
-    plot(weakConnections(i,:)','Color',[0.2 0.2 0.2 i/25]), hold on;
-end
-ylim([0 200])
 %% Trial by Trial analysis ##Only use with batch processed files##
 addpath(genpath('Figures'));
 [batchSpikes,batch_corr] = TrialByTrial(batchData([1,2,4])); % Function call
@@ -250,18 +80,17 @@ bin = 20;
 [z,mu,sigma] = zscore(sim_index);
 figure('Name','Cosine-Similarity Index'); h = htmp(sim_index,100);
 caxis([0 0.7]);
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);...
-     print(gcf,'-painters','-depsc2', 'Figures/CSTrials.eps', '-r250');
+set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);
  figure('Name','Dice Correlation')
  for i = 1:size(batch_corr,3)
      subplot(2,3,i),h = htmp(batch_corr(:,:,i),20);caxis([0 0.4]);
  end
 %% Plot all the Figures
 addpath('Figures');
-figure('Name','DeltaF/F'); stack_plot(DeltaFoverF,0.8,2); 
+figure('Name','DeltaF/F'); stack_plot(DeltaFoverF,1.5,25); 
 figure('Name','Convolved Spikes'); plot(dDeltaFoverF');
 figure('Name','Threshold Detection');DeltaFoverFplotter(dDeltaFoverF,std_threshold,static_threshold)
-figure('Name','Spike Plot'); spikePlot = Show_Spikes(Spikes);
+figure('Name','Spike Plot'); Show_Spikes(Spikes);
 % figure('Name','Temporal Shuffled Spike Plot'); shuffledTspikePlot = Show_Spikes(Spikes_shuffled);
 % figure('Name','Event Shuffled Spike Plot'); shuffledEspikePlot = Show_Spikes(Event_shuffled);
 % figure('Name','Total Shuffled Spike Plot'); shuffledAspikePlot = Show_Spikes(Total_shuffled);
