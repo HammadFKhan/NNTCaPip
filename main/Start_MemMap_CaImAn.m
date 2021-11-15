80cb0dda26b1ee4584b50c43bfe3f9cf268c5e60
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-11-15 12:32:22 -0500

    Create Start_MemMap_CaImAn.m

---
+++ main/Start_MemMap_CaImAn.m
@@ -0,0 +1,21 @@
+%%Define Global Variables
+global AverageImage
+global ROIcentroid
+global ROI
+global Image_Stack
+global num_images
+global DeltaFoverF
+global dDeltaFoverF
+global Noise_Power
+global files
+AverageImage = 0;
+ROIcentroid = 0;
+ROI = 0;
+Image_Stack = 0;
+num_images = 0;
+DeltaFoverF = 0;
+dDeltaFoverF = 0;
+Noise_Power = 0;
+files = 0;
+
+memMap_CaImAn;
