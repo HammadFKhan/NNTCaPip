b9eea1bbdfd0a31557bf14d8e9c993f1eee9b351
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-11-15 12:32:25 -0500

    Update tempShuffle.m

--- main/tempShuffle.m
+++ main/tempShuffle.m
@@ -1,11 +1,12 @@
 function Spikes_shuffled = tempShuffle(Spikes,surrogate)
-for ii = 1:surrogate
 cell_count = size(Spikes,1);
 num_images = size(Spikes,2);
-Spikes_shuffled = zeros(cell_count,num_images);
-for i = 1:cell_count
-    p = randperm(num_images);
-    Spikes_shuffled(i,:) = Spikes(i,p);
-end
+Spikes_shuffled = Spikes;
+for ii = 1:surrogate
+    disp(['Surrogate #: ' num2str(ii)])
+    for i = 1:cell_count
+        p = randperm(num_images);
+        Spikes_shuffled(i,:) = Spikes_shuffled(i,p);
+    end
 end
 
