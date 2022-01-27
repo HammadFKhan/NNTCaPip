144c9b6b5b82a67688abf1de78aa06fa46859a6f
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-01-27 15:03:32 -0500

    Create ensembleVid.m

---
+++ main/ensembleVid.m
@@ -0,0 +1,36 @@
+function ensembleVid(Ensemble,AverageImage,ROIcentroid,fn)
+if nargin<4 || strcmp('',fn)
+    fn = 'ensembleVideo';
+else
+    [~,name,~] = fileparts(fn)
+    fn = name;
+end
+
+set(0,'DefaultFigureWindowStyle','normal')
+clear mov
+writerobj = VideoWriter([fn '.avi'],'Uncompressed AVI'); % Initialize movie file
+writerobj.FrameRate = 8;
+open(writerobj);
+for i = 1:Ensemble.ensembleIndentified 
+    figure(i)
+    axis off
+    color = jet(Ensemble.ensembleIndentified);
+    EnsembleMap(AverageImage,ROIcentroid,Ensemble.NodeList{i},8,color(i,:))
+    set(gcf,'Position',[100 100 500 500])
+    drawnow
+    mov(i) = getframe(gcf);
+    writeVideo(writerobj,mov(i));
+end
+close(writerobj)
+disp('Video saved to current directory')
+% Displays ensemble overlay
+figure,
+
+for i = 1:Ensemble.ensembleIndentified 
+    axis off
+    color = jet(Ensemble.ensembleIndentified);
+    EnsembleMap(AverageImage,ROIcentroid,Ensemble.NodeList{i},8,color(i,:))
+    set(gcf,'Position',[100 100 500 500])
+    drawnow
+    hold on
+end
