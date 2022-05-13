dc52765963504b7b915b599bfb743024c440ef15
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-05-12 20:16:21 -0400

    Update STA.m

--- main/STA.m
+++ main/STA.m
@@ -1,13 +1,12 @@
-function calcium_avg = STA(DeltaFoverF,Spikes,std_threshold,window)
+function calcium_avg = STA(DeltaFoverF,std_threshold,window)
 
 %% find all spikes and align to the peak
 %find negative peaks in filtered data with an amplitude of at least 4 std and 1ms  in
 %separation
 
 %for each detected spike, extract a window 2ms before and after the peak
-[I,J] = find(Spikes);
 for i = 1:length(DeltaFoverF(:,1))
-    thresh = 2.5.*std(DeltaFoverF(i,:));
+    thresh = std_threshold.*std(DeltaFoverF(i,:));
     [pks,locs] = findpeaks(DeltaFoverF(i,:),'MinPeakHeight',thresh);
     spikeCount = 0;
     if(~isempty(pks))
@@ -16,7 +15,7 @@ for i = 1:length(DeltaFoverF(:,1))
             if(thisLoc-window>1 && thisLoc+window<length(DeltaFoverF))%%peak can't occur at the very end or very beginning of the data set
                 spikeCount = spikeCount+1;
                 %extract a 4ms window around the spike peak
-                 calcium_avg(:,spikeCount) = DeltaFoverF(thisLoc-window:thisLoc+window);
+                 calcium_avg(:,spikeCount) = DeltaFoverF(thisLoc-(window/5):thisLoc+window);
             end
         end
     end
