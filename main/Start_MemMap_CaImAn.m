9f8075c18b398c518d126040a674f396c95989ca
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-05-12 20:16:26 -0400

    Update Start_MemMap_CaImAn.m

--- main/Start_MemMap_CaImAn.m
+++ main/Start_MemMap_CaImAn.m
@@ -8,6 +8,9 @@ global DeltaFoverF
 global dDeltaFoverF
 global Noise_Power
 global files
+global A
+global C
+global ops
 AverageImage = 0;
 ROIcentroid = 0;
 ROI = 0;
@@ -17,5 +20,8 @@ DeltaFoverF = 0;
 dDeltaFoverF = 0;
 Noise_Power = 0;
 files = 0;
+A = [];
+C = [];
+ops = [];
 
 memMap_CaImAn;
