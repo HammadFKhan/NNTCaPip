9fa5227cfdc67ff17c2364c5cab80f9fa4cd0a55
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-05-12 20:15:46 -0400

    Update ensembleStat.m

--- main/ensembleStat.m
+++ main/ensembleStat.m
@@ -1,11 +1,11 @@
-function [ensembleSize,ensembleNum,ensembleRecruitment,minRecruitment,maxRecruitment] = ensembleStat(Ensemble,ROI)
+function [ensembleSize,ensembleNum,ensembleRecruitment,minRecruitment,maxRecruitment] = ensembleStat(Ensemble)
 % Ensemble stats for trial processing
 for i = 1:size(Ensemble.NodeList,2)
     ensembleSize(i) = length(Ensemble.NodeList{i});
 end
 ensembleSize(ensembleSize==1) = [];
 ensembleNum = length(ensembleSize);
-ensembleRecruitment = (mean(ensembleSize)/size(ROI,1));
-minRecruitment = (min(ensembleSize)/size(ROI,1));
-maxRecruitment = (max(ensembleSize)/size(ROI,1));
+ensembleRecruitment = (mean(ensembleSize)/max(cell2mat(Ensemble.NumNodes)));
+minRecruitment = (min(ensembleSize)/max(cell2mat(Ensemble.NumNodes)));
+maxRecruitment = (max(ensembleSize)/max(cell2mat(Ensemble.NumNodes)));
 
