443786e98a187686749b2cf87786cc94936659ca
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-07-18 20:21:55 -0400

    Update spikeState.m

--- main/spikeState.m
+++ main/spikeState.m
@@ -1,4 +1,4 @@
-function stateSpike = spikeState(Vel,Spikes,time,CaFR,state)
+function [stateSpike,stateSpikeFrame] = spikeState(Vel,Spikes,time,CaFR,state)
 if state
     velFlag = find(Vel(:,2)>=1); % Find indices of vel threshold
     disp('Analyzing Run states!')
@@ -11,6 +11,7 @@ idx = diff(velFlag); % Find where there are breaks across indices
 idx = find(idx>3); % Segment those breaks
 velFlagTime = linspace(0,time(end),length(Vel));
 velFlagTime = ceil(velFlagTime*CaFR); %Change to frames
+stateSpikeFrame = [];
 for i = 1:length(idx)+1
     if i==1 % ie. we want to include 1
         win = velFlag(1:idx(i));
@@ -20,24 +21,30 @@ for i = 1:length(idx)+1
         end
         if state && (win2(1)-ceil(5*CaFR))>=1 % If in running state include intiation phase
             stateSpike{i} = Spikes(:,(win2(1)-ceil(5*CaFR)):win2(end));
+            stateSpikeFrame = horzcat(stateSpikeFrame,(win2(1)-ceil(5*CaFR)):win2(end));
         else
             stateSpike{i} = Spikes(:,win2(1):win2(end));
+            stateSpikeFrame = horzcat(stateSpikeFrame,win2(1):win2(end));
         end
     elseif i ~=length(idx)+1
         win = velFlag(idx(i-1)+1:idx(i));
         win2 = velFlagTime(win(1):win(end));
         if state
             stateSpike{i} = Spikes(:,(win2(1)-ceil(5*CaFR)):win2(end));
+            stateSpikeFrame = horzcat(stateSpikeFrame,(win2(1)-ceil(5*CaFR)):win2(end));
         else
             stateSpike{i} = Spikes(:,win2(1):win2(end));
+            stateSpikeFrame = horzcat(stateSpikeFrame,win2(1):win2(end));
         end
     else % from the last break till the end of the segment
         win = velFlag(idx(i-1)+1:end);
         win2 = velFlagTime(win(1):win(end));
         if state
             stateSpike{i} = Spikes(:,(win2(1)-ceil(5*CaFR)):win2(end));
+            stateSpikeFrame = horzcat(stateSpikeFrame,(win2(1)-ceil(5*CaFR)):win2(end));
         else
             stateSpike{i} = Spikes(:,win2(1):win2(end));
+            stateSpikeFrame = horzcat(stateSpikeFrame,win2(1):win2(end));
         end
     end
 end
