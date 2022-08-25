98d69859fd3d85503bb2f1e62f51d04168acb925
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-08-25 14:56:57 -0400

    Update Spike_Detector_Single.m

--- main/Spike_Detector_Single.m
+++ main/Spike_Detector_Single.m
@@ -4,14 +4,15 @@ function Spikes = Spike_Detector_Single(DeltaFoverF,stddev,spikemin)
 Spikes = zeros(size(DeltaFoverF,1),size(DeltaFoverF,2),size(DeltaFoverF,3));
 
 % dev = stddev*std(std(DeltaFoverF(:,:,:)));
-dev = stddev*mean(std(DeltaFoverF(:,:,:),0,2));
 avghx = mean(mean(DeltaFoverF(:,:,:)));
 for j = 1:size(DeltaFoverF,1)
-    for k = 1:size(DeltaFoverF,2)
-        Spikes(j,k) = 0;
-        if DeltaFoverF(j,k) >= dev + avghx && DeltaFoverF(j,k) >= spikemin
-            Spikes(j,k) = 1;
-        else 
-        end
-    end
+    tempSpikes = Spikes(j,:);
+    dev = stddev*mean(std(DeltaFoverF(j,:,:)));
+    temp = DeltaFoverF(j,:);
+    tempSpikes(temp>= dev + avghx & temp >= spikemin) = 1;
+    Spikes(j,:) = tempSpikes;
+%     if DeltaFoverF(j,k) >= dev + avghx && DeltaFoverF(j,k) >= spikemin
+%         Spikes(j,k) = 1;
+%     else
+%     end
 end
