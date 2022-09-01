49e33fda08434c3d265b458a456f022bcca40143
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-09-01 16:58:32 -0400

    Update ensembleStat.m

--- main/ensembleStat.m
+++ main/ensembleStat.m
@@ -1,5 +1,4 @@
-function [ensembleSize,ensembleNum,ensembleRecruitment,minRecruitment,maxRecruitment,...
-    rankedActivityCoords,activityCentroid,activityCentroidVariance] = ensembleStat(Ensemble)
+function [Ensemble] = ensembleStat(Ensemble)
 % Ensemble stats for trial processing
 for i = 1:size(Ensemble.NodeList,2)
     ensembleSize(i) = length(Ensemble.NodeList{i});
@@ -13,3 +12,12 @@ rankedActivityCoords = Ensemble.rankedActivityCoords;
 
 activityCentroid = vertcat(Ensemble.ActivityCentroid{:});
 activityCentroidVariance = vertcat(Ensemble.ActivityCentroidVariance{:});
+% Output to structure
+Ensemble.ensembleSize = ensembleSize;
+Ensemble.ensembleNum = ensembleNum;
+Ensemble.ensembleRecruitment = ensembleRecruitment;
+Ensemble.minRecruitment = minRecruitment;
+Ensemble.maxRecruitment = maxRecruitment;
+Ensemble.rankedActivityCoords = rankedActivityCoords;
+Ensemble.activityCentroid = activityCentroid;
+Ensemble.activityCentroidVariance = activityCentroidVariance;
