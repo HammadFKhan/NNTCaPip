0205fa2f0513852ab8b29e977f23d85d53347748
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-10-14 13:19:41 -0400

    Update EnsembleStats2.m

--- Pipelines/EnsembleStats2.m
+++ Pipelines/EnsembleStats2.m
@@ -9,8 +9,7 @@ for i = 1:L
     StrName=fieldnames(S);
     StrName = StrName{1};
 %     [P_c{i},I_c{i},H_c(i,1),H_e(i,1),sizeE{i},sizeEdge{i}] = EntropyParser(S.(StrName));
-    [ensembleSize{i},ensembleNum{i},ensembleRecruitment{i},minRecruitment{i},maxRecruitment{i},...
-        rankedActivityCoords{i},activityCentroid{i},activityCentroidVariance{i}] = ensembleStat(S.(StrName));
+    [Ensemble(i).stat] = ensembleStat(S.(StrName));
 end 
 %%
 figure,bar(cell2mat(ensembleRecruitment))
