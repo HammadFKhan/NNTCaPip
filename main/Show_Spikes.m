7a005e2e5b91ef436202829f2da31efdb940c44e
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-01-26 13:05:24 -0500

    Update Show_Spikes.m

--- main/Show_Spikes.m
+++ main/Show_Spikes.m
@@ -1,8 +1,10 @@
 function Show_Spikes(Spikes)
 y = length(Spikes(:,1));
 x = length(Spikes(1,:));
+Fs = 30.048;
 spikePlot = zeros(y,x);
 baseline = 0;
+time = (1:x)/Fs;
 for i = 1:y
     for ii = 1:x
         if Spikes(i,ii) == 1
@@ -11,7 +13,7 @@ for i = 1:y
             spikePlot(i,ii) = NaN;
         end
     end
-    plot(spikePlot(i,:),'.','MarkerEdgeColor','k'); hold on;
+    plot(time,spikePlot(i,:),'.','MarkerEdgeColor','k'); hold on;
 %     plot_raster(spikePlot(i,:)); hold on;
     baseline = baseline + 1;
 end
