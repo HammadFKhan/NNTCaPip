a0876f2a0c567b125cd8a316c7bcbe7a5a2828dd
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-01-26 13:05:10 -0500

    Update centroidAnalysis.m

--- main/connectivityAnalysis/centroidAnalysis.m
+++ main/connectivityAnalysis/centroidAnalysis.m
@@ -4,12 +4,12 @@ function centroidAnalysis(Ensemble)
 centroidData = [];
 count = 1;
 for i = 1:Ensemble.ensembleIndentified
-    if ~ischar(Ensemble.ActivityCentroid{i})
-    centroidData(count,1) = Ensemble.ActivityCentroid{i}(1);
-    centroidData(count,2) = Ensemble.ActivityCentroid{i}(2);
-    centroidData(count,3) = Ensemble.ActivityCentroidVariance{i}(1);
-    centroidData(count,4) = Ensemble.ActivityCentroidVariance{i}(2);
-    count = count+1;
+    if ~isempty(Ensemble.ActivityCentroid{i})
+        centroidData(count,1) = Ensemble.ActivityCentroid{i}(1);
+        centroidData(count,2) = Ensemble.ActivityCentroid{i}(2);
+        centroidData(count,3) = Ensemble.ActivityCentroidVariance{i}(1);
+        centroidData(count,4) = Ensemble.ActivityCentroidVariance{i}(2);
+        count = count+1;
     end
 end
 
@@ -18,7 +18,7 @@ figure,errorbar(centroidData(:,1),centroidData(:,2),...
 legend
 %% For loop for all Centroids
 
-for i = 1:5
+for i = 1:length(allCentroidData)
     figure(i),box off
     %normalize activity location (0,0)
     data = allCentroidData{i};
@@ -36,8 +36,9 @@ end
 % hyp1 = sqrt(centroidFit(:,1).^2 + centroidFit(:,2).^2);
 % hyp2 = sqrt(centroidFit(:,3).^2 + centroidFit(:,4).^2);
 % figure,plot(mdl,'marker','o','Color','g'),legend off,box off,title('Euclidean Centroid Activity vs. Variance')
-for i = 1:5
-    hyp1 = sqrt(allCentroidData{i}(:,1).^2 + allCentroidData{i}(:,2).^2);
+for i = 1:length(allCentroidData)
+%     hyp1 = sqrt(allCentroidData{i}(:,1).^2 + allCentroidData{i}(:,2).^2);
+    
     hyp2 = sqrt(allCentroidData{i}(:,3).^2 + allCentroidData{i}(:,4).^2);
     mdl = fitlm(hyp1,hyp2);
     figure,plot(mdl,'marker','o','Color','k'),legend off,box off,hold on
