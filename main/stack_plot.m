764613160fc8354f3ec7fccd0a50e7b8db2f7fff
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-04-02 17:54:16 -0400

    Update stack_plot.m

--- main/stack_plot.m
+++ main/stack_plot.m
@@ -1,5 +1,8 @@
-function stack_plot(DeltaFoverF,spacing,amplitude)
-if nargin < 3 || strcmp(amplitude,'')
+function stack_plot(DeltaFoverF,spacing,amplitude,normFlag)
+if nargin < 4 || strcmp(normFlag,'')
+    normFlag = 0;
+end
+if nargin < 2 || strcmp(amplitude,'')
     amplitude = 1;
 end
 if nargin < 2 || strcmp(spacing,'')
@@ -10,7 +13,11 @@ Fs = 30.048;
 x = length(DeltaFoverF(1,:));
 y = length(DeltaFoverF(:,1));
 time = (1:x)/Fs;
-
+if normFlag
+    for i = 1:y
+        DeltaFoverF(i,:) = (DeltaFoverF(i,:)-min(DeltaFoverF(i,:)))/(max(DeltaFoverF(i,:))-min(DeltaFoverF(i,:)));
+    end
+end
 baseline = spacing*max(DeltaFoverF,[],'all');
 for i = 1:y
     gradient = i/y;
