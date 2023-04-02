1fed03a07dfa25e42e8ad7862775bf9cd866e19d
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-04-02 17:53:45 -0400

    Update htmp.m

--- main/htmp.m
+++ main/htmp.m
@@ -2,9 +2,10 @@ function h = htmp(corr,ticksize)
 if nargin<2 || isempty(ticksize); ticksize = 20; end
 
 h = imagesc(corr);
-% [grad,~]=colorGradient([1 1 1],[0 0 0],64);
-% colormap(grad);
-colormap(jet);
+[grad,~]=colorGradient([1 1 1],[46 127 187]/255,64);
+map = brewermap(64,'Blues');
+colormap(map);
+% colormap(jet);
 set(get(colorbar,'title'),'string','\Delta F/F (%)');
 axis on;axis tight;box off; axis xy;
 end
