eb76d09ed20d67458bb88d05f7414d9acd9d035a
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-04-02 17:53:32 -0400

    Update ensembleStat.m

--- main/connectivityAnalysis/ensembleStat.m
+++ main/connectivityAnalysis/ensembleStat.m
@@ -8,10 +8,21 @@ ensembleNum = length(ensembleSize);
 ensembleRecruitment = (mean(ensembleSize)/max(cell2mat(Ensemble.NumNodes)));
 minRecruitment = (min(ensembleSize)/max(cell2mat(Ensemble.NumNodes)));
 maxRecruitment = (max(ensembleSize)/max(cell2mat(Ensemble.NumNodes)));
-rankedActivityCoords = Ensemble.rankedActivityCoords;
 
-activityCentroid = vertcat(Ensemble.ActivityCentroid{:});
-activityCentroidVariance = vertcat(Ensemble.ActivityCentroidVariance{:});
+
+try % Stupid edge case when there is a string in cell (legacy noob build)
+    activityCentroid = vertcat(Ensemble.ActivityCentroid{:});
+    activityCentroidVariance = vertcat(Ensemble.ActivityCentroidVariance{:});
+catch
+    for i = 1:length(Ensemble.ActivityCentroid)
+        cBuff{i} = class(Ensemble.ActivityCentroid{i}); % Find char class cells
+        cvBuff{i} = class(Ensemble.ActivityCentroidVariance{i});
+    end
+    cIdx = find(~contains(cBuff,'char')); % Remove character cell arrays
+    cvIdx = find(~contains(cvBuff,'char'));
+    activityCentroid = vertcat(Ensemble.ActivityCentroid{cIdx}); % Do the analysis again
+    activityCentroidVariance = vertcat(Ensemble.ActivityCentroidVariance{cvIdx});
+end
 
 % Output to structure
 Ensemble.ensembleSize = ensembleSize;
@@ -19,6 +30,5 @@ Ensemble.ensembleNum = ensembleNum;
 Ensemble.ensembleRecruitment = ensembleRecruitment;
 Ensemble.minRecruitment = minRecruitment;
 Ensemble.maxRecruitment = maxRecruitment;
-Ensemble.rankedActivityCoords = rankedActivityCoords;
 Ensemble.activityCentroid = activityCentroid;
 Ensemble.activityCentroidVariance = activityCentroidVariance;
