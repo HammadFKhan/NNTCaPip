a0c4489c5ed6a5686aee5b069a2cb81ede1577fa
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-04-18 14:25:01 -0400

    Update Spike_Detector_Single.m

--- main/Spike_Detector_Single.m
+++ main/Spike_Detector_Single.m
@@ -4,7 +4,7 @@ function Spikes = Spike_Detector_Single(DeltaFoverF,stddev,spikemin)
 Spikes = zeros(size(DeltaFoverF,1),size(DeltaFoverF,2),size(DeltaFoverF,3));
 
 % dev = stddev*std(std(DeltaFoverF(:,:,:)));
-avghx = mean(mean(DeltaFoverF(:,:,:)));
+avghx = 0;%mean(mean(DeltaFoverF(:,:,:)));
 for j = 1:size(DeltaFoverF,1)
     tempSpikes = Spikes(j,:);
     dev = stddev*mean(std(DeltaFoverF(j,:,:)));
