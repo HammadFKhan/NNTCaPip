8de24929c8c0eff3ec0ff56288407c0bd344f922
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-11-01 14:29:06 -0400

    Update stack_plot.m

--- main/stack_plot.m
+++ main/stack_plot.m
@@ -8,11 +8,13 @@ end
 if nargin < 2 || strcmp(spacing,'')
     spacing = 1;
 end
-Fs = 30.048;
-
+% Fs = 30.048;
+Fs = 10.5759;
 x = length(DeltaFoverF(1,:));
 y = length(DeltaFoverF(:,1));
-time = (1:x)/Fs;
+DeltaFoverF = flip(DeltaFoverF);
+time = 1/Fs:1/Fs:x/Fs;
+[grad,~]=colorGradient([7 49 97]/255,[110 192 235]/255,y);
 if normFlag
     for i = 1:y
         DeltaFoverF(i,:) = (DeltaFoverF(i,:)-min(DeltaFoverF(i,:)))/(max(DeltaFoverF(i,:))-min(DeltaFoverF(i,:)));
@@ -21,7 +23,7 @@ end
 baseline = spacing*max(DeltaFoverF,[],'all');
 for i = 1:y
     gradient = i/y;
-    plot(time,amplitude*DeltaFoverF(i,:)+(spacing*baseline),'LineWidth',1,'Color',[.30 .835 .384]); hold on;
+    plot(time,amplitude*DeltaFoverF(i,:)+(spacing*baseline),'LineWidth',1,'Color',grad(i,:)); hold on; % ,'Color',[.30 .835 .384]
     baseline = baseline + spacing*max(DeltaFoverF,[],'all');
     axis tight, box off,...
             set(gca,'YTick','','YTickLabel','');
