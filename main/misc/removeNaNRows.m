fa2869dac8ef5883a2e313c576a01f43242c27db
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-11-17 15:22:56 -0500

    Delete removeNaNRows.m

--- main/misc/removeNaNRows.m
+++
@@ -1,7 +0,0 @@
-function newArray = removeNaNRows(array)
-    % Find rows containing NaN values
-    nanRows = any(isnan(array), 2);
-    
-    % Remove rows with NaN values
-    newArray = array(~nanRows, :);
-end
