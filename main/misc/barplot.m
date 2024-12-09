fb0cd96f21ce3e88aed0eb0c09b0cb7284e2c7f3
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2024-12-09 17:47:44 -0500

    Update barplot.m

--- main/misc/barplot.m
+++ main/misc/barplot.m
@@ -1,7 +1,18 @@
 function barplot(data)
 %plots std of boxplot from single dimensional data
-x = mean(data);
-bar(mean(data)),hold on
-y = 1;
-err = (std(data))*ones(size(x));
+x = nanmean(data);
+bar(nanmean(data)),hold on
+y = 1:length(ones(size(x)));
+err = (nanstd(data));
+buff = [];
+scatterOn = 1;
+if scatterOn
+    for i = 1:size(data,2)
+        t = data(:,i);
+        buff = [buff;t(t>0)];
+        labels = repmat({num2str(i)},length(buff),1);
+        scatter(i*ones(length(t(t>0)),1),t(t>0),'filled','jitter','on','jitterAmount',0.1)
+    end
+end
 errorbar(y,x,err)
+
